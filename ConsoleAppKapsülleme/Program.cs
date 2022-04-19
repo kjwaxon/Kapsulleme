83d68443042ee4983932fb0eb52fc9e0a8162653
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:46:57 +0300

    Kapsülleme

---
+++ ConsoleAppKapsülleme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsülleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Öğrenci ogr1 = new Öğrenci();
+            ogr1.setIsim("Kaan");
+            string gelen = ogr1.getIsim();
+
+            if(gelen== "Kaan")
+            {
+                Console.WriteLine("Başarılı");
+
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Console.ReadKey();
+                    
+        }
+    }
+}
---
+++ ConsoleAppKapsülleme/Öğrenci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsülleme
+{
+    class Öğrenci
+    {
+        public string isim;
+
+        public void setIsim(string _isim)
+        {
+            isim = _isim;
+        }
+
+        public string getIsim()
+        {
+            return isim;
+        }
+    }
+}
